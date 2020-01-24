98d71255fec624e542fa46cd2d1575ee65ce0d5e
zykzhangyukang <3053161401@qq.com>
2020-01-24 09:26:36 +0800

    完成草地的绘制

---
+++ src/beans/Ground.java
@@ -0,0 +1,58 @@
+package beans;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * 草地类
+ * Created by zhangyukang on 2020/1/24 08:37
+ */
+public class Ground extends Frame{
+
+    private static final int ROWS=50;//草地行数
+    private static final int COLS=50;//草地列数
+    private static final int BOX_SIZE=20;//每个小格的大小
+    private static final Color GROUND_COLOR=Color.GRAY;//草地的颜色
+
+
+    /**
+     * 草地运行
+     */
+    public void execute(){
+        setSize(COLS*BOX_SIZE,ROWS*BOX_SIZE);
+        setLocation(200,200);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                setVisible(false);
+                System.exit(0);
+            }
+        });
+        setVisible(true);
+    }
+
+    /**
+     * 画出草地
+     * @param g
+     */
+    @Override
+    public void paint(Graphics g) {
+        Color color = g.getColor();
+        g.setColor(GROUND_COLOR);
+        g.fillRect(0,0,COLS*BOX_SIZE,ROWS*BOX_SIZE);
+        //画出横线
+        g.setColor(Color.BLACK);
+        for(int i=1;i<ROWS;i++){
+            g.drawLine(0,i*BOX_SIZE,COLS*BOX_SIZE,i*BOX_SIZE);
+        }
+        for(int i=1;i<COLS;i++){
+            g.drawLine(i*BOX_SIZE,0,BOX_SIZE*i,ROWS*BOX_SIZE);
+        }
+        g.setColor(color);
+    }
+
+    public static void main(String[] args){
+        new Ground().execute();
+    }
+}
